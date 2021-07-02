653bfbcd4929e6222d66d9501cfbd46cdfd0352c
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-07-02 20:06:46 +0200

    Add expo-dev-client

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * @format
  */
+import 'expo-dev-client';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('main', () => App);
